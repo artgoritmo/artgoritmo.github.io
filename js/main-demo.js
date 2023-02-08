dbb0dd6f7c78d98e36ae2d53e8f65bc2b8646086
Artgoritmo <isc.villavicencio16@gmail.com>
2023-02-08 12:19:07 -0600

    Update main-demo.js

--- js/main-demo.js
+++ js/main-demo.js
@@ -3,7 +3,7 @@ $(function() {
     $('.style-options').addClass('active');
     setTimeout(function(){
         $('.style-options').removeClass('active');
-    }, 5000);
+    }, 2000);
 
     $('.style-options .toggle-btn').on('click', function() {
         $('.style-options').toggleClass('active');
