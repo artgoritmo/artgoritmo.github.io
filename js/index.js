6f7d17eb694a60abc383ebcf8fec5e4af50f7dc7
Arturo <amorales@laboratoriom.com>
2023-10-03 21:10:03 -0600

    se cambio la imagen de whatsapp

--- js/index.js
+++ js/index.js
@@ -154,3 +154,32 @@ links.forEach( link =>{
         }
     });
 });
+
+
+
+// Este es el método que vamos a llamar
+// cada vez que se detecte una intersección
+function onScrollEvent(entries, observer) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            var attributes = entry.target.attributes;
+            var src = attributes['data-src'].textContent;
+            entry.target.src = src;
+            entry.target.classList.add('visible');
+        }
+    });
+}
+
+// Utilizamos como objetivos todos los
+// elementos que tengan la clase lazyLoad,
+// que vimos en el HTML de ejemplo.
+var targets = document.querySelectorAll('.lazyLoad');
+
+// Instanciamos un nuevo observador.
+var observer = new IntersectionObserver(onScrollEvent);
+
+// Y se lo aplicamos a cada una de las
+// imágenes.
+targets.forEach(function(entry) {
+    observer.observe(entry);
+});
